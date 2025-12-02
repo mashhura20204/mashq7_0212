1ea60c872e8f30699306e3af5eae05de82628501
mashhura20204 <ibraimovamashhura0412@gmail.com>
2025-12-02 17:14:59 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# mashq7_0212
+# 16-misol
+
+year = lambda sana:sana.split("-")[0]
+print(year ("2025-12-04"))
+
+# 17-misol
+nechta = lambda lst:len(lst)
+print(nechta([1, 2, 3, 4 ]))
+
+# 18-misol
+
+harf = lambda a, b, c: min(a, b, c)
+print(harf(9, 3, 0))
+ 
+# 19-misol
+
+eng_kichik = lambda a, b, c:min(a, b, c)
+print(eng_kichik(4,13,57))
+
+# 20-misol
+
+numbers = lambda x: x + 1
+print(numbers(9))
